89383c3772ccd5b8878e2f1548b9b00aac14868d
ZXY <1483279495@qq.com>
2020-02-12 18:17:16 +0800

    vue practice

---
+++ build/webpack.config.practice.js
@@ -0,0 +1,71 @@
+const path = require('path')
+const HTMLPlugin = require('html-webpack-plugin')
+const webpack = require('webpack')
+const merge = require('webpack-merge')
+const baseConfig = require('./webpack.config.base')
+
+const defaultPlugins = [
+  new webpack.DefinePlugin({
+    'process.env': {
+      NODE_ENV: '"development"'
+    }
+  }),
+  new HTMLPlugin({
+    template: path.join(__dirname, 'template.html')
+  })
+]
+
+const devServer = {
+  port: 8080,
+  host: '0.0.0.0',
+  overlay: {
+    // 错误显示到网页上
+    errors: true
+  },
+  hot: true
+}
+let config
+
+config = merge(baseConfig, {
+  entry: path.join(__dirname, '../practice/index.js'),
+  devtool: '#cheap-module-eval-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.styl/,
+        use: [
+          'vue-style-loader',
+          'css-loader',
+          /* {
+            loader: 'css-loader',
+            options: {
+              module: true,
+              localIdentName: isDev ? '[path]-[name]-[hash:base64:5]' : '[hash:base64:5]',
+            }
+          }, */
+          // 和stylus-loader的顺序不能对调
+          {
+            loader: 'postcss-loader',
+            options: {
+              sourceMap: true
+            }
+          },
+          'stylus-loader'
+
+        ]
+      }
+    ]
+  },
+  devServer,
+  resolve: {
+    alias: {
+      'vue': path.join(__dirname, '../node_modules/vue/dist/vue.esm.js')
+    }
+  },
+  plugins: defaultPlugins.concat([
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoEmitOnErrorsPlugin()
+  ])
+})
+
+module.exports = config
---
+++ practice/instance/index.js
@@ -0,0 +1,7 @@
+import Vue from 'vue'
+
+const app = new Vue({
+  // el: '#root',
+  template: '<div>hello</div>'
+})
+app.$mount('#root')
